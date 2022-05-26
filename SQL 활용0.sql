7ad69e2c986b92ac2f5038991ad4fd8a24e93dd6
Seung-Bum <sb910126@gmail.com>
2022-05-26 16:42:08 +0900

    group by practice

--- SQL 활용0.sql
+++ SQL 활용0.sql
@@ -19,7 +19,8 @@ GROUP BY NM;
 
 --4
 SELECT NM
-      ,MAX(SCORE) SCORE
+      ,MAX(SCORE) SCORE 
+      -- MAX(SUM, MIN등 집계함수)는 GROUP BY와 함께 사용해야된다. 왜냐하면 단일 테이블이여야 하기 때문
       ,MAX(SCORE || SUBJECT) SUBJECT
 FROM STD_SCORE
 GROUP BY NM; -- GROUP BY시에는 컬럼을 따로 생각하자
