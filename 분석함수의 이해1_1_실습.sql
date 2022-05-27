e32376eac4dac8a59aa36222b5f52342072d6e93
Seung-Bum <sb910126@gmail.com>
2022-05-27 16:48:06 +0900

    20220527

--- 분석함수의 이해1_1_실습.sql
+++ 분석함수의 이해1_1_실습.sql
@@ -37,6 +37,55 @@ FROM (
  ORDER BY SEQ;
  */
  
+ -- 분석함수를 사용하지 않고 구현한 앞/뒤 데이터 차이 계산
+ 
+SELECT MAX( DECODE(NO, 1, NAME) ) NAME
+      ,SEQ
+      ,MIN( DECODE(NO, 1, KOR) ) KOR -- NO1 테이블 기본 정보들 출력
+      ,MIN( DECODE(NO, 1, ENG) ) ENG
+      ,MIN( DECODE(NO, 1, MAT) ) MAT
+      ,MIN( DECODE(NO, 1, KOR) ) + MIN( DECODE(NO, 1, ENG) ) + MIN( DECODE(NO, 1, MAT) ) TOT
+     
+FROM(
+	 SELECT NAME
+	       ,NO -- COPY_T를 1번 테이블, 2번 테이블로 구분함
+	       ,DECODE(NO, 1, SEQ, SEQ+1) SEQ 
+	       -- SEQ가 1이 아닐경우 SEQ를 1씩증가 시킴 ★★★ 여기서 이전행과 다음행이 구분
+	       ,CLASS
+	       ,KOR KOR
+	       ,ENG ENG
+	       ,MAT MAT
+	       ,DECODE(NO, 1, KOR) + DECODE(NO, 1, ENG) + DECODE(NO, 1, MAT) N1
+	       ,DECODE(NO, 2, KOR) + DECODE(NO, 2, ENG) + DECODE(NO, 2, MAT) N2
+	 FROM(
+		SELECT ROWNUM SEQ, A.*
+		FROM(
+			SELECT NAME
+			      ,CLASS
+			      ,KOR
+			      ,ENG
+			      ,MAT
+			FROM EXAM_RSLT A
+			ORDER BY KOR + ENG + MAT DESC ,NAME 
+		) A
+	 ),COPY_T B -- 테이블을 복사한다.
+	 WHERE NO <= 2 --테이블이 두개로 만듬
+)
+GROUP BY SEQ -- 중복되는 SEQ기준으로 행을 합침 (SEQ)
+HAVING MIN(DECODE(NO, 1, KOR)) IS NOT NULL 
+-- 이전행과 다음행을 합치다보니 NULL 발생하는데 IS NOT NULL(NULL이 아니면 TURE)로 없애버림
+ORDER BY SEQ
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  
  
